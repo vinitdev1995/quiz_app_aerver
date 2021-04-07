fee32cc8ff019c17c907500c0d0b1510434247c3
Vinit <devanivinitkumar@gmail.com>
2021-04-07 16:59:24 +0530

    add authentication JWT token.

---
+++ src/app/auth/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const moment = require("moment");
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.decode(token, {complete: true});
+        const isValidRole = decoded.payload.Roles.includes("ROLE_ADMIN");
+        const expireTime = decoded.payload.exp;
+        const currentTime = moment().unix();
+        if ((expireTime > currentTime) && isValidRole) {
+            res.status(200);
+            next();
+        } else {
+            res.status(401).send({error: 'Something went wrong'})
+        }
+    } catch (e){
+        res.status(400).send({
+            error: new Error('Invalid request!')
+        });
+    }
+};
--- src/app/companyProfile/router.js
+++ src/app/companyProfile/router.js
@@ -2,8 +2,9 @@ require("./modal");
 const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
+const verifyToken = require("../auth/middleware");
 
-router.post("/Id/:id/addProfileDetails", controller.create);
+router.post("/Id/:id/addProfileDetails", verifyToken, controller.create);
 router.get("/getCompanyProfileDetails/Company/Id/:id", controller.findAll);
 router.get("/Public/:companyName", controller.findAll);
 
